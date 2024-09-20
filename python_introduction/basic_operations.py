62d96751b186147e0962e98fc58ea4debe82af3b
Nmb1171 <nahombadebo72@gmail.com>
2024-09-20 14:13:00 +0300

    Python intro

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 15;
+number2 = 25;
+
+print(f'The Sum of {number1} and {number2} is {number1 + number2}')
+print(f'THe subtraction of {number2} from {number1} is {number1 - number2}')
+print(f'THe multiplication of {number1} multiplied by {number2} is {number1*number2}');
