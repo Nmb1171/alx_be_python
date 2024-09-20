1c00592c4a3e1a913561c631e48ab8cc751dbad8
Nmb1171 <nahombadebo72@gmail.com>
2024-09-20 14:54:10 +0300

    Initial Commit

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+# Capture the user's age as input (as a string)
+age_str = input('How old are you? ')
+
+# Convert the input to an integer
+age = int(age_str)
+
+# Add 27 to the age
+age += 27
+
+# Display the prophecy
+print(f'In 2050, you will be {age} years old.')
