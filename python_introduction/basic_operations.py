1e36cd174540360a0f8b177c87e42beeee534cb5
Nmb1171 <nahombadebo72@gmail.com>
2024-09-20 14:16:00 +0300

    Python intro

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,5 @@
-number1 = 15;
-number2 = 25;
+number1 = 10;
+number2 = 5;
 
 print(f'The Sum of {number1} and {number2} is {number1 + number2}')
 print(f'THe subtraction of {number2} from {number1} is {number1 - number2}')
