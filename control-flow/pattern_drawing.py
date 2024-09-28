7b2332a870d83428557af6a98789c33100cf1620
Nmb1171 <nahombadebo72@gmail.com>
2024-09-29 00:03:19 +0300

    Multiplication table

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+# Get the size of the square pattern
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()  # Move to the next line after each row
+    row += 1
