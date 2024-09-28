609190d9b6e4c60f5e3ee9a6e79ee42b2aa49a5f
Nmb1171 <nahombadebo72@gmail.com>
2024-09-28 23:59:30 +0300

    Multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+# Get the number from the user
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and print the multiplication table
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
