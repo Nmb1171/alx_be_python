935c8d54c315706cbfa3590cd5bab2c5f12c57a6
Nmb1171 <nahombadebo72@gmail.com>
2024-09-29 16:20:44 +0300

    Added arthimethic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,7 @@
-def perform_operation (num1,num2,operation):
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
+    """Performs basic arithmetic operations based on the provided operation parameter."""
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -7,7 +10,7 @@ def perform_operation (num1,num2,operation):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            return "Error: Division by Zero is not allowed"
+            return "Error: Division by zero is not allowed"
         return num1 / num2
     else:
-        return "Invalid Operation"
+        return "Invalid operation"
